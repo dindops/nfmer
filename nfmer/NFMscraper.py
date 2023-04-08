884645d134813d5ceb98f07c27e7a1e1a75ceb66
Piotr Dindorf <piotr.dindorf@mailo.com>
2023-04-08 23:59:14 +0200

    Adds and implements retrieve_event_program function

--- nfmer/NFMscraper.py
+++ nfmer/NFMscraper.py
@@ -22,8 +22,26 @@ def retrieve_all_events_links(url):
     return events
 
 
+def retrieve_event_program(soup):
+    program_tag = soup.find('div', class_="nfmArtAITitle", string="Program:")
+    try:
+        program_raw = program_tag.find_next().text.replace("***", "")
+        #  TODO: try to figure out how to distinguish authors from their work
+    except AttributeError:
+        # AttributeError means that event's program is not yet established
+        program_raw = ""
+    return program_raw
+
+
 
 nfm_events = retrieve_all_events_links(NFM_URL)
+
+for event in nfm_events.keys():
+    event_url = nfm_events[event]["url"]
+    response = requests.get(event_url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    program = retrieve_event_program(soup)
+    nfm_events[event]["program"] = program
 
 TODO:
 * parse over each link, and get data about Program, date, location
