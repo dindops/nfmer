4892a25c3522b20bef3c029583ab83ec7240de31
Piotr Dindorf <piotr.dindorf@mailo.com>
2023-04-08 23:57:53 +0200

    Refactors existing code into a single function

--- nfmer/NFMscraper.py
+++ nfmer/NFMscraper.py
@@ -3,24 +3,28 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://www.nfm.wroclaw.pl/component/nfmcalendar'
-response = requests.get(url)
+NFM_URL = "https://www.nfm.wroclaw.pl/component/nfmcalendar"
 
-soup = BeautifulSoup(response.content, 'html.parser')
 
-events = {}
+def retrieve_all_events_links(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    events = {}
+    for section in soup.find_all("a", class_="nfmEDTitle"):
+        title = section.contents[0].strip()
+        href = section["href"]
+        event_id = href.split("/")[-1]
+        event_url = f"{url}/event/{event_id}"
+        events[event_id] = {
+                "title": title,
+                "url": event_url
+                }
+    return events
 
-for section in soup.find_all('a', class_='nfmEDTitle'):
-    title = section.contents[0].strip()
-    href = section['href']
-    event_id = href.split('/')[-1]
-    event_url = f"{url}/event/{event_id}"
-    events[event_id] = {
-            'title': title,
-            'url': event_url
-            }
 
-'''
+
+nfm_events = retrieve_all_events_links(NFM_URL)
+
 TODO:
 * parse over each link, and get data about Program, date, location
 * confirm if this really retrieves all events
