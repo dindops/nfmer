b1d93457543ddd47996c106fc93a8f54a3ef2490
Piotr Dindorf <piotr.dindorf@mailo.com>
2023-10-06 13:29:10 +0200

    Creates ScraperDBHandler class for all db-related behaviors that scraper would require

--- nfmer/scraper/db_handler.py
+++ nfmer/scraper/db_handler.py
@@ -8,85 +8,97 @@ import asyncio
 DB_FILE = "nfm_events.db"
 NFM_URL = "https://www.nfm.wroclaw.pl/component/nfmcalendar"
 
-def initialize_database():
-    conn = sqlite3.connect(DB_FILE)
-    cursor = conn.cursor()
+class ScraperDBHandler:
+    def __init__(self, events_list: list):
+        __db_location = "nfm_events.db"
+        self.events_list = events_list
+        self.conn = sqlite3.connect(__db_location)
+        self._initialize_database()
+        self.existing_events_ids = self._get_list_of_event_ids()
 
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS nfm_events (
-            event_id INTEGER PRIMARY KEY,
-            url TEXT,
-            event_programme TEXT,
-            location TEXT,
-            date TEXT
-        )
-    ''')
-
-    conn.commit()
-    conn.close()
-
-
-def get_list_of_event_ids() -> set:
-    conn = sqlite3.connect(DB_FILE)
-    cursor = conn.cursor()
-    cursor.execute('SELECT event_id FROM nfm_events')
-    rows = cursor.fetchall()
-    event_ids = {row[0] for row in rows}
-    conn.close()
-    return event_ids
-
-
-def insert_event_data(event):
-    conn = sqlite3.connect(DB_FILE)
-    cursor = conn.cursor()
-
-    cursor.execute('''
-        INSERT INTO nfm_events (event_id, url, event_programme, location, date)
-        VALUES (?, ?, ?, ?, ?)
-    ''', (
-        event.event_id,
-        event.url,
-        str(event.event_programme),
-        event.location,
-        event.date
-    ))
-
-    conn.commit()
-    conn.close()
-
-def update_event_data(event):
-    conn = sqlite3.connect(DB_FILE)
-    cursor = conn.cursor()
-    cursor.execute('''
-        UPDATE nfm_events
-        SET url = ?, event_programme = ?, location = ?, date = ?
-        WHERE event_id = ?
-    ''', (
-        event.url,
-        str(event.event_programme),  # Convert dictionary to string
-        event.location,
-        event.date,
-        event.event_id
-    ))
-    conn.commit()
-    conn.close()
-
-
-def handle_saving_events_to_db(events_list) -> None:
-    existing_event_ids = get_list_of_event_ids()
-    for event in events_list:
-        if event.event_id in existing_event_ids:
-            update_event_data(event)
+    def __exit__(self, exc_value):
+        if isinstance(exc_value, Exception):
+            self.conn.rollback()
         else:
-            insert_event_data(event)
+            self.conn.commit()
+        self.conn.close()
+
+    def _initialize_database(self) -> None:
+        self.conn.execute('''
+            CREATE TABLE IF NOT EXISTS nfm_events (
+                event_id INTEGER PRIMARY KEY,
+                url TEXT,
+                event_programme TEXT,
+                location TEXT,
+                date TEXT
+            )
+        ''')
+        self.conn.commit()
+
+    def _get_list_of_event_ids(self) -> set:
+        cursor = self.conn.cursor()
+        cursor.execute('SELECT event_id FROM nfm_events')
+        rows = cursor.fetchall()
+        event_ids = {row[0] for row in rows}
+        return event_ids
+
+
+    def _insert_event_data(self, event):
+        cursor = self.conn.cursor()
+        cursor.execute('''
+            INSERT INTO nfm_events (event_id, url, event_programme, location, date)
+            VALUES (?, ?, ?, ?, ?)
+        ''', (
+            event.event_id,
+            event.url,
+            str(event.event_programme),
+            event.location,
+            event.date
+        ))
+        self.conn.commit()
+
+    def _update_event_data(self, event):
+        cursor = self.conn.cursor()
+        cursor.execute('''
+            UPDATE nfm_events
+            SET url = ?, event_programme = ?, location = ?, date = ?
+            WHERE event_id = ?
+        ''', (
+            event.url,
+            str(event.event_programme),  # Convert dictionary to string
+            event.location,
+            event.date,
+            event.event_id
+        ))
+        self.conn.commit()
+
+    def save_events_to_db(self) -> None:
+        for event in self.events_list:
+            if event.event_id in self.existing_events_ids:
+                self._update_event_data(event)
+            else:
+                self._insert_event_data(event)
+        self.conn.close()
 
 
 async def main():
-    scraper = Scraper(NFM_URL)
+    nfm_events_urls = [
+            # "https://www.nfm.wroclaw.pl/component/nfmcalendar/event/10060",
+            # "https://www.nfm.wroclaw.pl/component/nfmcalendar/event/10164",
+            # "https://www.nfm.wroclaw.pl/component/nfmcalendar/event/10102"
+            "https://www.nfm.wroclaw.pl/component/nfmcalendar/event/10346"
+            ]
+    scraper = Scraper(*nfm_events_urls)
     await scraper.scrape()
     nfm_events = scraper.event_soup
-    initialize_database()
-    handle_saving_events_to_db(nfm_events)
+    db_handler = ScraperDBHandler(nfm_events)
+    db_handler.save_events_to_db()
+
+    # TODO:
+    # * update procedure:
+    #   * check if event data is being updated correctly  DONE
+    #   * behavioral class?
+    # * move execution to scraper.py
 
 if __name__ == "__main__":
     asyncio.run(main())
