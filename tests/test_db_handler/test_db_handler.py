eb39669ef20ea82fa63a091b8b70372dfeb4d67e
Piotr Dindorf <piotr.dindorf@mailo.com>
2025-03-29 15:20:03 +0100

    chore: adds missing db_handler unit tests

--- tests/test_db_handler/test_db_handler.py
+++ tests/test_db_handler/test_db_handler.py
@@ -2,11 +2,11 @@ from datetime import date
 
 from sqlmodel import Session
 
-from nfmer.db_handler import DatabaseHandler
+from nfmer.db_handler import DatabaseHandler, get_db
 from nfmer.models import NFM_Event
 
 
-def test_save_new_event(db_handler: DatabaseHandler, mock_events_dict: dict[str, NFM_Event]) -> None:
+def test_save_and_retrieve_events(db_handler: DatabaseHandler, mock_events_dict: dict[str, NFM_Event]) -> None:
     with Session(db_handler.engine):
         db_handler.save_event_data(mock_events_dict)
         saved_events = db_handler.get_all_events()
@@ -15,6 +15,16 @@ def test_save_new_event(db_handler: DatabaseHandler, mock_events_dict: dict[str,
         assert composition_by_event[0].composition_name == "Symphony No. 7"
         composition_by_composer = db_handler.get_compositions_by_composer("Snoop Dogg")
         assert composition_by_composer[0].composition_name == "Gin and Juice"
+        all_compositions = db_handler.get_all_compositions()
+        assert len(all_compositions) == 2
+        gin_and_juice = db_handler.search_compositions_by_name("gin and juice")
+        composition2 = db_handler.get_composition_by_id(2)
+        assert gin_and_juice[0] == composition2
+        all_composers = db_handler.get_all_composers()
+        assert len(all_composers) == 2
+        snoop_dogg = db_handler.search_composers_by_name("snoop dogg")
+        composer2 = db_handler.get_composer_by_id(2)
+        assert composer2 == snoop_dogg[0]
 
 
 def test_update_existing_event(db_handler: DatabaseHandler, mock_events_dict: dict[str, NFM_Event]) -> None:
@@ -36,3 +46,9 @@ def test_update_existing_event(db_handler: DatabaseHandler, mock_events_dict: di
     compositions = db_handler.get_compositions_by_event("1")
     assert len(compositions) == 1
     assert compositions[0].composition_name == "squabble up"
+
+
+def test_get_db() -> None:
+    db_generator = get_db()
+    db = next(db_generator)
+    assert isinstance(db, DatabaseHandler)
