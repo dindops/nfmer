aacc9436393b2dcb3f93629e4bd987b5b47d9256
Piotr Dindorf <piotr.dindorf@mailo.com>
2026-02-01 13:02:44 +0100

    feat: adds a simple favicon

--- nfmer/api/v1/api.py
+++ nfmer/api/v1/api.py
@@ -1,7 +1,12 @@
 import uvicorn
+from pathlib import Path
+
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from nfmer.api.v1.routers import composers, compositions, events, pages
+
+STATIC_DIR = Path(__file__).parent.parent / "static"
 
 api = FastAPI(title="NFMer API", version="v1")
 
@@ -9,6 +14,8 @@ api.include_router(events.router)
 api.include_router(compositions.router)
 api.include_router(composers.router)
 api.include_router(pages.router)
+
+api.mount("/static", StaticFiles(directory=str(STATIC_DIR)), name="static")
 
 if __name__ == "__main__":
     uvicorn.run("api:api", host="0.0.0.0", port=8000, reload=True)
